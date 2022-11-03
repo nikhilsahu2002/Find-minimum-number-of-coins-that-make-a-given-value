5cab3f8c6a99cf0248ae6f5c9cc6841545a678cf
nikhilsahu2002 <117375980+nikhilsahu2002@users.noreply.github.com>
2022-11-03 21:02:04 +0530

    Update MINIMUMC.C

--- MINIMUMC.C
+++ MINIMUMC.C
@@ -3,7 +3,7 @@
 void main ()
 {
 int amount=0,b=0,c=0;
-int coin5 ,coin1;
+int coin_of_5 ,coin_of_1;
 clrscr();
 printf("Enter the amount");
 scanf("%d",&amount);
@@ -11,8 +11,8 @@ printf("Enter the Number of  5 rupee coin ");
 scanf("%d",&b);
 printf("Enter the number of 1 rupee coin ");
 scanf("%d",&c);
-coin5 = amount/5;
-coin1 = amount%5;
+coin_of_5 = amount/5;
+coin_of_1 = amount%5;
 if (coin5 > b )
 	printf("-1");
 if (coin1 > c)
