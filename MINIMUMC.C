3a23ae362ea3175dcf13ac39bb03f6bb32505dcb
nikhilsahu2002 <117375980+nikhilsahu2002@users.noreply.github.com>
2022-11-17 00:45:01 +0530

    Update MINIMUMC.C

--- MINIMUMC.C
+++ MINIMUMC.C
@@ -11,14 +11,17 @@ printf("Enter the Number of  5 rupee coin ");
 scanf("%d",&b);
 printf("Enter the number of 1 rupee coin ");
 scanf("%d",&c);
-coin_of_5 = amount/5;
-coin_of_1 = amount%5;
-if (coin_of_5 > b || coin_of_1 >c )
+if(amount<= c)
+{
+	coin_of_1 = amount/1;
+	printf("%d",coin_of_1);
+}
+else if (coin_of_5 > b || coin_of_1 >c )
 	printf("-1");
-//if (coin1 > c)
-//	printf("-1");
 else
 {
+	coin_of_5 = amount/5;
+	coin_of_1 = amount%5;
 	printf("The number of 5 rupee coin need is %d \n",coin_of_5);
 	printf("The Number of 1 repee coin need is %d \n",coin_of_1);
 }
