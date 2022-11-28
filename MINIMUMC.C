9fde709080a2e60666f9d443ed780e49a6019362
nikhilsahu2002 <117375980+nikhilsahu2002@users.noreply.github.com>
2022-11-28 11:45:48 +0530

    Update MINIMUMC.C

--- MINIMUMC.C
+++ MINIMUMC.C
@@ -2,7 +2,7 @@
 #include<conio.h>
 void main ()
 {
-int amount=0,b=0,c=0;
+int amount=0,b=0,c=0,flag1=0,flag2=0;
 int coin_of_5 ,coin_of_1;
 clrscr();
 printf("Enter the amount");
@@ -11,19 +11,21 @@ printf("Enter the Number of  5 rupee coin ");
 scanf("%d",&b);
 printf("Enter the number of 1 rupee coin ");
 scanf("%d",&c);
-if(amount<= c)
-{
-	coin_of_1 = amount/1;
-	printf("%d",coin_of_1);
-}
-else if (coin_of_5 > b || coin_of_1 >c )
-	printf("-1");
-else
-{
-	coin_of_5 = amount/5;
-	coin_of_1 = amount%5;
-	printf("The number of 5 rupee coin need is %d \n",coin_of_5);
-	printf("The Number of 1 repee coin need is %d \n",coin_of_1);
-}
+	flag1=b*5;
+	flag2=c*amount;
+	if(amount<=flag1 && amount<=flag2)
+	{
+		coin_of_5 = amount/5;
+		coin_of_1 = amount%5;
+		printf("The Five Rupee Coin Need Is %d \n",coin_of_5);
+		printf("The One Rupee Coin Need Is %d \n",coin_of_1);
+	}
+	else if (amount<=c)
+	{
+		coin_of_1 = amount/1;
+		printf("The One Rupee Needed %d",coin_of_1);
+	}
+	else
+		printf("-1");
 getch();
 }
